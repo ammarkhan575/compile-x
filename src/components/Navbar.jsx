4cb0f7f85a1cc43e4c97e80a19f338018db12d56
Mohd. Ammar <mohd.ammar@squareboat.com>
2024-09-18 15:53:08 +0530

    REMOVED: login and signup button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center px-2 bg-[#0F0F0F] py-2 text-white">
-      <div>Compile-X</div>
+      <div className="text-[#29C244] ">Compile-X</div>
       <div>
         <button className="bg-[#2F2F2F] px-4 py-2 rounded-md text-[#29C244]">
           {" "}
@@ -11,8 +11,8 @@ const Navbar = () => {
         </button>
       </div>
       <div className="flex gap-4">
-        <button className="bg-blue-400 px-4 py-2 rounded-md">Login</button>
-        <button className="bg-blue-400 px-4 py-2 rounded-md">Signup</button>
+        {/* <button className="bg-blue-400 px-4 py-2 rounded-md">Login</button>
+        <button className="bg-blue-400 px-4 py-2 rounded-md">Signup</button> */}
       </div>
     </div>
   );
