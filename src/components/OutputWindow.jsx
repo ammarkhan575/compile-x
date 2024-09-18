4f9993500c01cb39ce87f4e1f9da8fce39aa9805
Mohd. Ammar <mohd.ammar@squareboat.com>
2024-09-19 00:59:13 +0530

    ADD: text and logo for output

--- src/components/OutputWindow.jsx
+++ src/components/OutputWindow.jsx
@@ -34,10 +34,26 @@ const OutputWindow = ({ outputDetails }) => {
   };
   return (
     <>
-      <h1 className="font-semibold text-white text-xl bg-clip-text text-transparent bg-gradient-to-r from-slate-900 to-slate-700 mb-2">
-        Output
-      </h1>
       <div className="outline-2 outline outline-black w-full bg-panelbody h-96 bg-[#1e293b] rounded-md text-white font-normal text-sm overflow-y-auto">
+        <div className="border-b border-b-black rounded-t-md flex items-center gap-2 pl-2 py-2">
+          <span className="w-4 h-4">
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="far"
+              data-icon="terminal"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 576 512"
+            >
+              <path
+                fill="#29C244"
+                d="M6.3 72.2c-9-9.8-8.3-24.9 1.4-33.9s24.9-8.3 33.9 1.4l184 200c8.5 9.2 8.5 23.3 0 32.5l-184 200c-9 9.8-24.2 10.4-33.9 1.4s-10.4-24.2-1.4-33.9L175.4 256 6.3 72.2zM248 432H552c13.3 0 24 10.7 24 24s-10.7 24-24 24H248c-13.3 0-24-10.7-24-24s10.7-24 24-24z"
+              ></path>
+            </svg>
+          </span>
+          Output
+        </div>
         {outputDetails && <>{getOutput()}</>}
       </div>
     </>
